253a9aabe3c7da4cdf629cbd9f6297630104515a
waldo121 <17034517+waldo121@users.noreply.github.com>
2025-12-05 08:53:42 -0500

    Improve day4 solution

--- 04/main.py
+++ 04/main.py
@@ -51,27 +51,24 @@ def find_rolls(grid, grid_size) -> List[tuple[int,int]]:
                 rolls.append((index_x, index_y))
     return rolls
 
-def get_removable_cells(rolls: List[tuple[int,int]], removed_cells: List[tuple[int,int]] = []) -> List[tuple[int,int]]:
+def get_removable_cells(rolls: List[tuple[int,int]]) -> List[tuple[int,int]]:
     removable: List[tuple[int,int]] = []
     for roll_position in rolls:
         index_x,index_y = roll_position[0], roll_position[1]
-        if grid[index_x][index_y] == '.' or (index_x,index_y) in removed_cells:
-            continue
         #count accessible rolls
         adjacents = 0
         adjacent_cells = get_adjacent_cells(index_x,index_y)
         for cell in adjacent_cells:
-            if grid[cell[0]][cell[1]] == '@' and not cell in removed_cells:
+            if (cell[0], cell[1]) in rolls:
                 adjacents += 1
         if adjacents < 4:
             removable.append((index_x, index_y))
     return removable
 rolls = find_rolls(grid=grid, grid_size=grid_size)
+initial_length = len(rolls)
 removable_cells = get_removable_cells(rolls=rolls)
-removed_cells = []
-
 while len(removable_cells) != 0:
-    removed_cells += removable_cells
-    removable_cells = get_removable_cells(rolls=rolls, removed_cells=removed_cells)
-print("removed :", len(removed_cells))
-# note: I don't know for sure how long it took, but around 10-15min max, solution might be further optimisable
+    rolls = [r for r in rolls if r not in removable_cells]
+    removable_cells = get_removable_cells(rolls=rolls)
+print("removed :", initial_length-len(rolls))
+# note: ran in 2m20 on my laptop
